280e5c9ddd95b593007087bc4df11f7b96dcaf51
Hugo Caldarola <hugo_caldarolahugo@reseau.eseo.fr>
2022-12-17 23:51:35 +0100

    [feat] Add javadoc + design

--- src/main/java/com/controller/ApplicationController.java
+++ src/main/java/com/controller/ApplicationController.java
@@ -1,9 +1,5 @@
 package com.controller;
 
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.List;
-
-import org.apache.catalina.filters.ExpiresFilter.XServletOutputStream;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,34 +18,36 @@ import com.service.VilleService;
 @Controller
 public class ApplicationController {
 
+	private static final String VILLES = "villes";
+
 	@Autowired
 	public VilleService villeService;
 
+
 	@GetMapping(value = "/")
 	public String home(@ModelAttribute VilleDTO villeADTO, @ModelAttribute VilleDTO villeBDTO, Model model) throws Exception {
-		List<Ville> villes = villeService.getVilles();
-		model.addAttribute("villes", villes);
-		
+		model.addAttribute(VILLES, villeService.getVilles());
+
 		return "home";
 	}
-	
+
 	@PostMapping(value = "/")
 	public String postDistance(Model model, @ModelAttribute("villeA") String strVilleA, @ModelAttribute("villeB") String strVilleB) throws Exception {
-		model.addAttribute("villes", villeService.getVilles());
-		
+		model.addAttribute(VILLES, villeService.getVilles());
+
 		try {
 			String codeCommuneVilleA = StringUtils.substringBetween(strVilleA, "codeCommune=", ", nomCommune");
 			String codeCommuneVilleB = StringUtils.substringBetween(strVilleB, "codeCommune=", ", nomCommune");
-	
+
 			Ville villeA = villeService.getVilleByCodeCommune(codeCommuneVilleA);
 			Ville villeB = villeService.getVilleByCodeCommune(codeCommuneVilleB);
-		
+
 			String distance = villeService.getDistance(villeA, villeB);
-		
+
 			model.addAttribute("villeA", villeA);
 			model.addAttribute("villeB", villeB);
 			model.addAttribute("distance", distance);
-			
+
 		} catch (JSONException e) {
 			if (e.getMessage() != null) {
 				model.addAttribute("error", "Veillez à sélectionner deux villes de la liste pour le calcul de distance.");
@@ -64,40 +62,39 @@ public class ApplicationController {
 
 	@GetMapping(value = "/listVilles")
 	public String listVilles(Model model) throws Exception {
-		List<Ville> villes = villeService.getVilles();
-		model.addAttribute("villes", villes);
-		
+		model.addAttribute(VILLES, villeService.getVilles());
+
 		return "listVilles";
 	}
-	
+
 	@GetMapping(value = "/listVilles/{codeCommune}")
 	public String getVilleModify(@PathVariable(value = "codeCommune") String codeCommune, Model model) throws Exception {
 		Ville ville = villeService.getVilleByCodeCommune(codeCommune);
 		Coordonnee coordonnee = ville.getCoordonnee();
-		
+
 		model.addAttribute("ville", ville);
 		model.addAttribute("coordonnee", coordonnee);
-		
+
 		return "villeModify";
 	}
-	
+
 	@PostMapping(value = "/listVilles/{codeCommune}")
 	public String postVilleModify(Model model, @ModelAttribute(value = "codeCommune") String codeCommune, @ModelAttribute("nomCommune") String nomCommune, @ModelAttribute("codePostal") String codePostal, @ModelAttribute("libelleAcheminement") String libelleAcheminement, @ModelAttribute("ligne") String ligne, @ModelAttribute("latitude") String latitude, @ModelAttribute("longitude") String longitude) throws Exception {
 		Ville ville = villeService.getVilleByCodeCommune(codeCommune);
 		Coordonnee coordonnee = ville.getCoordonnee();
-				
+
 		model.addAttribute("ville", ville);
 		model.addAttribute("coordonnee", coordonnee);
 
 		villeService.putVille(codeCommune, nomCommune, codePostal, libelleAcheminement, ligne, latitude, longitude);
-		
+
 		return "redirect:/listVilles";
 	}
-	
+
 	@PostMapping(value = "/listVilles/{codeCommune}/delete")
-    public String postVilleDelete(@PathVariable String codeCommune) throws Exception {
-    	villeService.deleteVille(codeCommune);
-        
-        return "redirect:/listVilles";
-    }
+	public String postVilleDelete(@PathVariable String codeCommune) throws Exception {
+		villeService.deleteVille(codeCommune);
+
+		return "redirect:/listVilles";
+	}
 }
--- src/main/java/com/service/VilleService.java
+++ src/main/java/com/service/VilleService.java
@@ -15,13 +15,16 @@ import com.google.gson.JsonParser;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import com.model.Coordonnee;
 import com.model.Ville;
 
 @Service
 public class VilleService {
 
+	/**
+	 * @return The list of all towns
+	 * @throws Exception
+	 */
 	public List<Ville> getVilles() throws Exception {
 		List<Ville> villes = new ArrayList<>();
 
@@ -60,14 +63,16 @@ public class VilleService {
 		return villes;
 	}
 	
+	/**
+	 * @param codeCommune
+	 * @return The ville which is linked to the codeCommune
+	 * @throws Exception
+	 */
 	public Ville getVilleByCodeCommune(String codeCommune) throws Exception {
-		// Host url
 		String host = "http://localhost:8181/ville/";
 
-		// Format query for preventing encoding problems
 		HttpResponse<JsonNode> response = Unirest.get(host + codeCommune).asJson();
 
-		// Prettifying
 		Gson gson = new Gson();
 		JsonParser jp = new JsonParser();
 		JsonElement je = jp.parse(response.getBody().toString());
@@ -91,8 +96,18 @@ public class VilleService {
 		return ville;
 	}
 	
+	/**
+	 * @param codeCommune
+	 * @param nomCommune
+	 * @param codePostal
+	 * @param libelleAcheminement
+	 * @param ligne
+	 * @param latitude
+	 * @param longitude
+	 * @return A string which confirm the update
+	 * @throws Exception
+	 */
 	public String putVille(String codeCommune, String nomCommune, String codePostal, String libelleAcheminement, String ligne, String latitude, String longitude) throws Exception {
-		// Host url
 		String host = "http://localhost:8181/ville/put";
 		
 		String strCodeCommune = "?codeCommune=" + codeCommune;
@@ -105,45 +120,49 @@ public class VilleService {
 		
 		String request = host + strCodeCommune + strNomCommune + strCodePostal + strLibelleAcheminement + strLigne + strLatitude + strLongitude;
 
-		// Format query for preventing encoding problems
-		HttpResponse<String> response = Unirest.put(request).asString();
+		Unirest.put(request).asString();
 		
-		System.out.println(request);
-
-		return "BREAKING NEWS : La ville a été correctemment modifiée !";
+		return "La ville a été correctement modifiée !";
 	}
 
+	/**
+	 * @param codeCommune
+	 * @return A string which confirms the deletion of the town
+	 * @throws Exception
+	 */
 	public String deleteVille(String codeCommune) throws Exception {
 		String host = "http://localhost:8181/ville/delete";
 		
 		String strCodeCommune = "?codeCommune=" + codeCommune;
 		
-		HttpResponse<String> response = Unirest.delete(host + strCodeCommune).asString();
+		Unirest.delete(host + strCodeCommune).asString();
 		
-		System.out.println(host + strCodeCommune);
-		
-		return "BREAKING NEWS : La ville a été corresctement supprimée !";
+		return "La ville a été correctement supprimée !";
 	}
 	
+	/**
+	 * @param villeA
+	 * @param villeB
+	 * @return The distance bewteen villeA and villeB calculed thanks to this:
+	 * 	R*arcos(sina*sinb + cosa*cosb*cos(c-d))
+		R = rayon de la Terre
+		a = latitude villeA / b = latitude villeB
+		c = longitude villeA / d = longitude villeB
+	 */
 	public String getDistance(Ville villeA, Ville villeB) {
-		// R*arcos(sina*sinb + cosa*cosb*cos(c-d))
-		// R = rayon de la Terre
-		// a = latitude villeA / b = latitude villeB
-		// c = longitude villeA / d = longitude villeB
 		double latitudeA = Float.parseFloat(villeA.getCoordonnee().getLatitude()) * Math.PI / 180;
 		double latitudeB = Float.parseFloat(villeB.getCoordonnee().getLatitude()) * Math.PI / 180;
 
 		double longitudeA = Float.parseFloat(villeA.getCoordonnee().getLongitude()) * Math.PI / 180;
 		double longitudeB = Float.parseFloat(villeB.getCoordonnee().getLongitude()) * Math.PI / 180;
 
-		double R = 6372.795477598;
+		double rayonTerre = 6372.795477598;
 
-		double distance = R * Math.acos((Math.sin(latitudeA) * Math.sin(latitudeB))
+		double distance = rayonTerre * Math.acos((Math.sin(latitudeA) * Math.sin(latitudeB))
 				+ (Math.cos(latitudeA) * Math.cos(latitudeB) * Math.cos(longitudeA - longitudeB)));
 
 		String format = "0.00";
 		NumberFormat formatter = new DecimalFormat(format);
-		String distanceString = formatter.format(distance);
-		return distanceString;
+		return formatter.format(distance);
 	}
 }
